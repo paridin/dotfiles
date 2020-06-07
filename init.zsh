556855f1d2e0d796e0ec4d51bd9bbc91dba0f108
Roberto Estrada <restrada@paridin.com>
2020-06-07 15:06:46 -0500

    support for user bin

--- init.zsh
+++ init.zsh
@@ -1,5 +1,7 @@
 source ${HOME}/.dotfiles/paridin-prompt/paridin-prompt.zsh-theme
 
+export PATH=$HOME/.local/bin:$PATH
+
 # Secret configs
 if [[ -s "${HOME}/.dotfiles/secret.zsh" ]]; then
   source "${HOME}/.dotfiles/secret.zsh"
